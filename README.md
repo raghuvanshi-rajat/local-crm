e969b5c2cfde1082d4f3393a6510009b445b5ed7
raghuvanshi-rajat <therajatraghuvanshi@gmail.com>
2024-08-12 22:06:38 +0530

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+1. required packages: `pip install -r requirements.txt`
+2. migrations: `python manage.py migrate`
+3. server: `python manage.py runserver`
---
+++ website/apps.py
@@ -0,0 +1,14 @@
+"""
+path: website/apps.py
+description: Website app configuration
+"""
+
+from django.apps import AppConfig
+
+
+class WebsiteConfig(AppConfig):
+    """
+    Website app configuration class
+    """
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "website"
